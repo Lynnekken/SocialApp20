bb0a800110c08a9785486a42eb978c9fbd609b69
Lynnekken <124069998+Lynnekken@users.noreply.github.com>
2023-04-19 22:04:42 +0200

    Componente Hifsangebote/gesuche erstellt

---
+++ Frontend/src/Components/SeekAndFind/Angebot.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Input from "../Input/Input";
+import InputDropdownR from "../Input/InputDropdownR";
+import Heading from "../Heading/Heading";
+
+const Angebot = () => {
+
+    return (
+      <div className="Angebot">
+        <Heading title={"Hilfsangebote"} className={"Heading1"}/> 
+        <Input name={"Titel:"} placeholder={"Geben Sie hier einen kurzen und prägnanten Titel ein"}/>
+        <Input name={"Ort:"} placeholder={"Wo benötigen Sie Hilfe?"}/>
+        <Input name={"Zeitraum:"} placeholder={"Geben Sie den Zeitraum so präsize wie möglich ein."}/>
+        <InputDropdownR name={"Katastrophe"} dropDownTitle={"Auswahl"} dropDown1={"Stromausfall"} dropDown2={"Hochwasser"} dropDown3={"Starkregen"}/>
+        <Input name={"Angebot:"}/>
+        <Input name={"E-Mail/Telefon:"} placeholder={"Geben Sie hier Ihre bevorzugte Anrt der Kontaktaufnahme an."}/>
+        <Input name={"Beschreibung:"} placeholder={"Beschreiben Sie hier präzise, was Sie anbieten."}/>
+      </div>
+    )
+  }
+
+  export default Angebot
--- Frontend/src/js/App.jsx
+++ Frontend/src/js/App.jsx
@@ -1,20 +1,19 @@
 //Components
-
+import Gesuche from '../Components/SeekAndFind/Gesuche'
+import Angebot from '../Components/SeekAndFind/Angebot'
 
 //CSS
 import '../scss/App.css'
 import '../scss/index.css'
 
-
-
 function App() {
-
 
   return (
     <div className="App">
-      <h1>Test</h1>
+      <Gesuche/>
+      <Angebot/>
     </div>
-  )
+  );
 }
 
 export default App
