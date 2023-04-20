34e8973cac2084e7c22ac023a0f95d3618b84456
Tape90 <119531778+Tape90@users.noreply.github.com>
2023-04-20 08:58:29 +0200

    Connected Database to Backend

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,37 @@
+require('dotenv').config();
 const express = require ("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
-const port = 3001;
+const port = process.env.Port;
 
 app.use(cors());
+app.use(express.json());
 
+const Database = "sample_analytics"
+
+app.use(async function (req, res, next) {
+    await mongoose.connect(`${process.env.Connection}${Database}?retryWrites=true&w=majority`)
+    next();
+});
+
+const testSchema = new mongoose.Schema({
+    account_id: Number,
+    limit: Number
+});
+
+const Account = mongoose.model('accounts', testSchema);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello')
+});
+
+app.get('/get', async(req, res) => {
+    const test = await Account.find({});
+    res.send(test);
+})
 
 app.listen(port, () => {
     console.log(`Server is running on Port ${port}`);
