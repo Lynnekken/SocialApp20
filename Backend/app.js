46a424018347658721e7632fc4f68286e80c8796
Tape90 <119531778+Tape90@users.noreply.github.com>
2023-04-20 14:44:50 +0200

    add get and post for database

--- Backend/app.js
+++ Backend/app.js
@@ -9,29 +9,72 @@ const port = process.env.Port;
 app.use(cors());
 app.use(express.json());
 
-const Database = "sample_analytics"
+const Database = "kat-net";
 
 app.use(async function (req, res, next) {
     await mongoose.connect(`${process.env.Connection}${Database}?retryWrites=true&w=majority`)
     next();
 });
 
-const testSchema = new mongoose.Schema({
-    account_id: Number,
-    limit: Number
+const userSchema = new mongoose.Schema({
+    firstname: String,
+    lastname: String,
+    email: String,
+    passwort: String,
+    job: String,
+    region: String,
+    skills: String,
+    equipment: String,
+    radius: Number,
+    available: String,
+    contact: String
 });
 
-const Account = mongoose.model('accounts', testSchema);
+const User = mongoose.model('user', userSchema);
+
+
+const seekorfindSchema = new mongoose.Schema({
+    type: String,
+    titel: String,
+    location: String,
+    period: String,
+    catastrophe: String,
+    contact: String,
+    description: String
+});
+
+const Offer = mongoose.model('seekOrFind', seekorfindSchema)
 
 
 app.get('/', (req, res) => {
     res.send('Hello')
 });
 
-app.get('/get', async(req, res) => {
-    const test = await Account.find({});
-    res.send(test);
-})
+app.get('/getuser', async(req, res) => {
+    const users = await User.find({});
+    res.send(users);
+});
+
+app.post('/postuser', async(req, res) => {
+    // const user = req.body oder req.query / Hier Daten entgegen nehmen
+    await User.create(user);
+    res.status(200).send("Datenbank akt.");
+});
+
+app.get('/getoffer', async(req, res) => {
+    const offer = await Offer.find({});
+    res.send(offer);
+});
+
+app.post('/postoffer', async(req, res) => {
+    //const offer = Hier Daten entgegen nehmen
+    await Offer.create(offer);
+    res.status(200).send("Datenbank akt.");
+});
+
+//app.delelte für löschen
+
+//app.put zum aktualisieren
 
 app.listen(port, () => {
     console.log(`Server is running on Port ${port}`);
